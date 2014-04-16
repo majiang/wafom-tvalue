5f408b863f71bb5a1734e563576d29d39950675a
majiang <r.97all+git@gmail.com>
2014-04-16 19:17:19 +0900

    usage

--- w2_infty_one.d
+++ w2_infty_one.d
@@ -9,14 +9,16 @@ void main(string[] args)
     import std.array;
 
     args.popFront();
-    foreach (dimensionF2; args[1].to!size_t() .. args[2].to!size_t())
+    if (args.length < 4)
+        return "usage: w2_infty_one dimR dimBmin dimBmax count".writeln();
+    foreach (dimensionF2; args[1].to!size_t() .. args[2].to!size_t()+1)
     {
         auto P = sobolPointSet!uint(Precision(32), DimensionR(args[0].to!size_t()), DimensionF2(dimensionF2));
         foreach (i; 0..args[3].to!size_t())
         {
             auto sP = P.scrambleRandomly();
             auto Q = sP[0], s = sP[1];
-            "%s,%(%b%),%.15f,%.15f".writefln(Q, s, Q.bimswafom().lg(), Q.bimsnrtwafom().lg());
+            "%s,%(%b%),%.15f,%.15f".writefln(Q, s, Q.bipmswafom().lg(), Q.bimsnrtwafom().lg());
         }
     }
 }
