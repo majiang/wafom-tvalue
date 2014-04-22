0ed906016642bbb80375ae7bb5a220159b434e91
majiang <r.97all+git@gmail.com>
2014-04-22 13:12:33 +0900

    changing one bit

--- w2_infty_one.d
+++ w2_infty_one.d
@@ -1,6 +1,7 @@
 import lib.newsobol;
 import lib.pointsettype;
 import lib.wafom;
+import lib.scramble;
 import std.math : lg  = log2;
 
 void main(string[] args)
@@ -13,12 +14,15 @@ void main(string[] args)
         return "usage: w2_infty_one dimR dimBmin dimBmax count".writeln();
     foreach (dimensionF2; args[1].to!size_t() .. args[2].to!size_t()+1)
     {
-        auto P = sobolPointSet!uint(Precision(32), DimensionR(args[0].to!size_t()), DimensionF2(dimensionF2));
-        foreach (i; 0..args[3].to!size_t())
-        {
-            auto sP = P.scrambleRandomly();
-            auto Q = sP[0], s = sP[1];
-            "%s,%(%b%),%.15f,%.15f".writefln(Q, s, Q.bipmswafom().lg(), Q.bimsnrtwafom().lg());
-        }
+        auto B = randomize(sobolBasis!uint(Precision(32), DimensionR(args[0].to!size_t()), DimensionF2(dimensionF2)));
+        auto P = ShiftedBasisPoints!uint(B, Precision(32));
+        "%s,%.15f,%.15f".writefln(P, P.bipmswafom().lg(), P.bimsnrtwafom().lg());
+        foreach_reverse (n; 0..P.precision)
+            foreach (s; 0..P.dimensionR)
+            {
+                auto sP = ShiftedBasisPoints!uint(modify(B, s, n), Precision(32));
+                "%s,%.15f,%.15f".writefln(sP, sP.bipmswafom().lg(), sP.bimsnrtwafom().lg());
+                modify(B, s, n);
+            }
     }
 }
