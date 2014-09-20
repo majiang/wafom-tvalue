a794374ad0800b3b85e89dce75a541bc1ae311b3
majiang <neuron.ohori@gmail.com>
2014-09-20 17:07:09 +0900

    add usage; memoize genzFactory

--- mmds-genz.d
+++ mmds-genz.d
@@ -5,7 +5,7 @@ import std.stdio;
 import lib.pointsettype;
 import lib.integral;
 import lib.wafom;
-import lib.testfunction;
+//import lib.testfunction;
 import std.conv : to;
 import std.string;
 import std.math;
@@ -25,17 +25,26 @@ auto rms(T)(T xs)
     return (sumsq / count).lg() / 2;
 }
 
+auto genzFactory(F)(size_t genzIndex, size_t dimensionR, in F coeff)
+{
+    import lib.testfunction : _gf = genzFactory;
+    import std.functional : memoize;
+    alias gf = memoize!(_gf!F);
+    return gf(genzIndex, dimensionR, coeff);
+}
+
 void main(string[] args)
 {
-    immutable numDS = args[2].to!size_t();
-    auto f = (args.length < 4) ?
-        args[1].to!size_t().genzFactory!real(4) :
-        args[1].to!size_t().genzFactory!real(4, args[3].to!real)
-    ;
-    stderr.writeln(f.I);
+    if (args.length < 3)
+        return "mmds-genz genzIndex numDS [coeff]".writeln();
+    immutable
+        genzIndex = args[1].to!size_t(),
+        numDS = args[2].to!size_t(),
+        coeff = (3 < args.length) ? args[3].to!real() : 1;
     foreach (line; stdin.byLine())
     {
         auto P = line.fromString!U();
+        auto f = genzIndex.genzFactory(P.dimensionR, coeff);
         debug if (10 < P.dimensionF2) break;
         real[] e;
         foreach (i; 0..numDS)
