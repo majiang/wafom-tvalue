ba902b2035b18a8cbd4891da8fa1e8501568d485
majiang <neuron.ohori@gmail.com>
2014-09-26 12:56:43 +0900

    simple sample

---
+++ compute_wafom.d
@@ -0,0 +1,17 @@
+import ui.input, lib.wafom, lib.pointsettype;
+import std.stdio;
+import std.math : lg = log2;
+
+void main()
+{
+	stderr.writeln(
+"Computes Walsh Figure of Merit:
+    Matsumoto-Saito-Matoba original (Dick weight)
+    Non-RMS
+input:
+    http://www.ms.u-tokyo.ac.jp/~ohori/format.html
+output:
+    lg(W(P)) on each line");
+	foreach (P; getDigitalNets!uint())
+		"%.15f".writefln(P.biwafom().lg());
+}
