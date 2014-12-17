1dbeae3eae342385c227df3c697c4001f61bd1c0
majiang <neuron.ohori@gmail.com>
2014-12-17 16:01:32 +0900

    given triples (P,c,W), choose good ones

---
+++ filter-good.d
@@ -0,0 +1,25 @@
+module optimize.filter;
+
+import std.container : heapify;
+import std.typecons : Tuple, tuple;
+alias Tuple!(real, string) D;
+
+void main(string[] args)
+{
+	import std.stdio;
+	import std.string : strip;
+	import std.array : split;
+	import std.conv : to;
+	if (args.length < 2)
+		return stderr.writeln("filter-good capacity");
+	auto capacity = args[1].to!size_t();
+	auto heap = (new D[capacity]).heapify(0);
+	foreach (line; stdin.byLine())
+	{
+		string value = line.dup;
+		real key = line.strip().split(",")[2].to!real();
+		heap.conditionalInsert(D(key, value));
+	}
+	foreach (t; heap.release())
+		t[1].write();
+}
